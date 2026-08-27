1cc7af8f011ad2f2d841bd6c6ad6838e605e62b5
Rebel1745 <31040719+Rebel1745@users.noreply.github.com>
2026-08-27 21:04:52 +0100

    Brain update
    Finished a crude (but working) version of the brain. I would like it committed before trying to optimise it

--- Traffic/Assets/_Scripts/Agents/Pedestrian/PedestrianBrain.cs
+++ Traffic/Assets/_Scripts/Agents/Pedestrian/PedestrianBrain.cs
@@ -17,6 +17,7 @@ public class PedestrianBrain : MonoBehaviour, IAgentBrain
         Goal backupGoal = new WalkToWaypointGoal(PedestrianManager.Instance.GetRandomPedestrianWaypoint(WaypointType.PedestrianWalkway));
         WaypointNode targetNode = null;
         BuildingBase target = null;
+        bool goHome = false;
 
         // figure out where we want to go
         if (rand < 0.1)
@@ -24,6 +25,7 @@ public class PedestrianBrain : MonoBehaviour, IAgentBrain
             // go home
             target = BuildingManager.Instance.GetBuilding(RelationshipManager.Instance.GetHomeBuildingsForPerson(agent.Id).First());
             targetNode = target.InsideBuildingWaypoint;
+            goHome = true;
         }
         else if (rand < 0.65f)
         {
@@ -73,71 +75,99 @@ public class PedestrianBrain : MonoBehaviour, IAgentBrain
         }
         else
         {
-            // do we need to use the car
-            // get closest car park
-            List<EntityId> carParks = BuildingManager.Instance.GetBuildingsByType(BuildingSubState.CarPark);
-            EntityId closestCarParkId = EntityId.None;
-            // if there is no car park, walk to the target
-            if (carParks.Count == 0)
-            {
-                if (targetNode != null)
-                    goalList.AddLast(new WalkToWaypointGoal(targetNode));
-                else goalList.AddLast(backupGoal);
+            AgentController vehicle = VehicleManager.Instance.GetVehicle(vehicleId);
 
-                Debug.Log("No car parks, walking");
+            if (goHome)
+            {
+                // we have a car and have to go home
+                if (agent.GetComponent<PedestrianMovement>().CurrentVehicle == null)
+                    goalList.AddLast(new WalkToAndEnterVehicleGoal(vehicle: vehicle));
+
+                goalList.AddLast(new DriveHomeGoal());
+                goalList.AddLast(new ExitVehicleGoal());
+                goalList.AddLast(new WalkToWaypointGoal(targetNode));
             }
             else
             {
-                if (carParks.Count == 1)
-                {
-                    closestCarParkId = carParks[0];
-                }
-                else
-                    closestCarParkId = BuildingManager.Instance.GetClosestBuildingToPosition(carParks, targetNode.Position);
+                // do we need to use the car
+                // get closest car park
+                List<EntityId> carParks = BuildingManager.Instance.GetBuildingsByType(BuildingSubState.CarPark);
+                EntityId closestCarParkId = EntityId.None;
+                // where are we parked?
+                EntityId currentBuildingId = RelationshipManager.Instance.GetBuildingFromParkingSpot(vehicle.Mover.CurrentWaypoint.Id).First();
 
-                AgentController vehicle = VehicleManager.Instance.GetVehicle(vehicleId);
-                BuildingCarPark carPark = BuildingManager.Instance.GetBuilding(closestCarParkId) as BuildingCarPark;
-
-                // get distance to the car
-                float distToCar = Utils.GetDistanceWithSetHeight(agent.Mover.CurrentWaypoint.Position, vehicle.Mover.CurrentWaypoint.Position, 0f);
-                // get distance from the car to the car park
-                float distToCarPark = Utils.GetDistanceWithSetHeight(vehicle.Mover.CurrentWaypoint.Position, carPark.PropertyEntryNode.Position, 0f);
-                // get distance from the car park to the target
-                float distFromCarParkToTarget = Utils.GetDistanceWithSetHeight(carPark.PropertyEntryNode.Position, targetNode.Position, 0f);
-                // get distance from the pedestrians current waypoint to the target
-                float distToTarget = Utils.GetDistanceWithSetHeight(agent.Mover.CurrentWaypoint.Position, targetNode.Position, 0f);
-                // comapre the distances (maybe weighted in some way). longer to drive? walk, otherwise drive
-                float distTotal = distToCar + distToCarPark + distFromCarParkToTarget;
-
-                Debug.Log($"Person: {agent.Mover.CurrentWaypoint.Position}. Car {vehicle.Mover.CurrentWaypoint.Position}. CP: {carPark.PropertyEntryNode.Position}. Target ({target.BuildingType}) {targetNode.Position}.");
-                Debug.Log($"To car: {distToCar}. To CP: {distToCarPark}. CP to target {distFromCarParkToTarget}. Total: {distTotal}. Person to target {distToTarget}.");
-
-                if (distTotal > (distToTarget * 20f))
+                if (carParks.Count == 0)
                 {
                     if (targetNode != null)
                         goalList.AddLast(new WalkToWaypointGoal(targetNode));
                     else goalList.AddLast(backupGoal);
 
-                    Debug.Log("Quicker walking. Do it!");
+                    Debug.Log("No car parks, walking");
                 }
                 else
                 {
-                    if (targetNode != null)
+                    if (carParks.Count == 1)
                     {
-                        // if we aren't already in a car, get in the car
-                        if (agent.GetComponent<PedestrianMovement>().CurrentVehicle == null)
-                            goalList.AddLast(new WalkToAndEnterVehicleGoal(vehicle: vehicle));
-
-                        // drive to the car park
-                        goalList.AddLast(new DriveToWaypointGoal(carPark.PropertyEntryNode, "Driving to car park entrance"));
-                        goalList.AddLast(new ParkInCarParkGoal(carPark));
-                        goalList.AddLast(new WaitGoal(2f));
-                        goalList.AddLast(new ExitVehicleGoal());
-                        goalList.AddLast(new WalkToWaypointGoal(targetNode));
-
-                        Debug.Log("We're driving to the car park and then walking to the target");
+                        closestCarParkId = carParks[0];
                     }
-                    else goalList.AddLast(backupGoal);
+                    else
+                        closestCarParkId = BuildingManager.Instance.GetClosestBuildingToPosition(carParks, targetNode.Position);
+
+                    if (closestCarParkId.Equals(currentBuildingId))
+                    {
+                        if (targetNode != null)
+                            goalList.AddLast(new WalkToWaypointGoal(targetNode));
+                        else goalList.AddLast(backupGoal);
+
+                        Debug.Log("We are already in the car park, walk");
+                    }
+                    else
+                    {
+                        BuildingCarPark carPark = BuildingManager.Instance.GetBuilding(closestCarParkId) as BuildingCarPark;
+
+                        // get distance to the car
+                        float distToCar = Utils.GetDistanceWithSetHeight(agent.Mover.CurrentWaypoint.Position, vehicle.Mover.CurrentWaypoint.Position, 0f);
+                        // get distance from the car to the car park
+                        float distToCarPark = Utils.GetDistanceWithSetHeight(vehicle.Mover.CurrentWaypoint.Position, carPark.PropertyEntryNode.Position, 0f);
+                        // get distance from the car park to the target
+                        float distFromCarParkToTarget = Utils.GetDistanceWithSetHeight(carPark.PropertyEntryNode.Position, targetNode.Position, 0f);
+                        // get distance from the pedestrians current waypoint to the target
+                        float distToTarget = Utils.GetDistanceWithSetHeight(agent.Mover.CurrentWaypoint.Position, targetNode.Position, 0f);
+                        // comapre the distances (maybe weighted in some way). longer to drive? walk, otherwise drive
+                        float distTotal = distToCar + distToCarPark + distFromCarParkToTarget;
+
+                        Debug.Log($"Person: {agent.Mover.CurrentWaypoint.Position}. Car {vehicle.Mover.CurrentWaypoint.Position}. CP: {carPark.PropertyEntryNode.Position}. Target ({target.BuildingType}) {targetNode.Position}.");
+                        Debug.Log($"To car: {distToCar}. To CP: {distToCarPark}. CP to target {distFromCarParkToTarget}. Total: {distTotal}. Person to target {distToTarget}.");
+
+                        // check to see if the distance to drive is further than the distance to walk (multiplied by some factor to represent the extra time taken to walk)
+                        if (distTotal > (distToTarget * 20f))
+                        {
+                            if (targetNode != null)
+                                goalList.AddLast(new WalkToWaypointGoal(targetNode));
+                            else goalList.AddLast(backupGoal);
+
+                            Debug.Log("Quicker walking. Do it!");
+                        }
+                        else
+                        {
+                            if (targetNode != null)
+                            {
+                                // if we aren't already in a car, get in the car
+                                if (agent.GetComponent<PedestrianMovement>().CurrentVehicle == null)
+                                    goalList.AddLast(new WalkToAndEnterVehicleGoal(vehicle: vehicle));
+
+                                // drive to the car park
+                                goalList.AddLast(new DriveToWaypointGoal(carPark.PropertyEntryNode, "Driving to car park entrance"));
+                                goalList.AddLast(new ParkInCarParkGoal(carPark));
+                                goalList.AddLast(new WaitGoal(2f));
+                                goalList.AddLast(new ExitVehicleGoal());
+                                goalList.AddLast(new WalkToWaypointGoal(targetNode));
+
+                                Debug.Log("We're driving to the car park and then walking to the target");
+                            }
+                            else goalList.AddLast(backupGoal);
+                        }
+                    }
                 }
             }
         }
